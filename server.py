0cdcad0f692e4d3bcb5188c11220abfd8174db65
Cory Lynch <cjl2625@gmail.com>
2016-12-29 18:10:02 -0500

    added actions for werewolf/tanner

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from random import shuffle
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 players = {}
+center = []
 
 #this variable was breaking, so making it global as lazy fix
 global host
@@ -42,12 +43,33 @@ def start_submit():
 	p = list(players)
 	shuffle(p)
 	for index,role in enumerate(settings):
-		players[p[index]] = role
+		if index>=len(players):
+			center.append(role)
+		else:
+			players[p[index]] = role
 	return redirect('/lobby/' + host)
 
-#Lobby pings here every once in a while to check status
+#Lobby pings here every once in a while to check what role you have
 @app.route('/check/<name>')
 def check(name):
 	return ('{"players": '+str(len(players))+', "role":"'+str(players[name])+'"}')
+
+#Send out forms to the users to take actions
+@app.route('/action/<name>')
+def action(name):
+	role = players[name]
+	if 'werewolf' in role:
+		return render_template('roles/werewolf.html',
+			wolf=", ".join([p for p in players if 'werewolf' in players[p]]),
+			name=name)
+	if role == 'tanner':
+		return '<p>To continue, <a href="timer/{name}">click here</a></p>'.format(name=name)
+
+	return render_template('roles/'+role+'.html', game=players)
+
+#Countdown to the end of the game
+@app.route('/timer/<name>')
+def timer(name):
+	return render_template('timer.html', name=name)
 
 app.run(host="127.0.0.1", port=5000)
